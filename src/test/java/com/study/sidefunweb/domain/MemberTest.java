90c1c2751e3eb2a0f8fc2903b50e604700b3b574
Judalm Park (dami) <wnekfa1004@naver.com>
2023-02-17 23:49:41 +0900

    TEST UPDATE

--- src/test/java/com/study/sidefunweb/domain/MemberTest.java
+++ src/test/java/com/study/sidefunweb/domain/MemberTest.java
@@ -1,6 +1,10 @@
 package com.study.sidefunweb.domain;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
@@ -11,40 +15,42 @@ import javax.persistence.PersistenceContext;
 
 import java.util.List;
 
+import static com.study.sidefunweb.domain.QMember.*;
+
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
 public class MemberTest {
 
-    @PersistenceContext
-    EntityManager em;
+    private JPAQueryFactory queryFactory;
+
+    @Autowired
+    private EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        Member member3 = new Member("member3");
+        Member member4 = new Member("member4");
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 
     @Test
     public void testEntity() {
-//
-//        Member member1 = new Member("member1", 10);
-//        Member member2 = new Member("member2", 10);
-//        Member member3 = new Member("member3", 10);
-//        Member member4 = new Member("member4", 10);
-//
-//        em.persist(member1);
-//        em.persist(member2);
-//        em.persist(member3);
-//        em.persist(member4);
-//
-//
-//        //초기화
-//        em.flush();
-//        em.clear();
-//
-//
-//
-//        //확인
-//        List<Member> members = em.createQuery("select m from Member m",
-//                        Member.class)
-//                .getResultList();
-//        for (Member member : members) {
-//            System.out.println("member=" + member);
-//        }
+        List<String> result = queryFactory.select(member.member_name)
+                .from(member)
+                .where(member.member_name.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+
     }
 }
